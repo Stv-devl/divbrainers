b4ec515f7b0cede51f4e7c1991e2ffceefed382f
Stevan-Dev <107848175+Stv-devl@users.noreply.github.com>
2025-05-27 11:01:00 +0700

    add favicon, fast seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,56 @@ const instrumentSans = Instrument_Sans({
 });
 
 export const metadata: Metadata = {
-  title: 'DivBrainers',
-  description: 'Divbrainers is a platform for learning and practicing coding',
+  title: 'DivBrainers — Practice for Developer Interviews',
+
+  description:
+    'DivBrainers is an open-source platform that helps developers prepare for technical job interviews. It combines AI-driven interviews, a secure dashboard, and upcoming tools like coding challenges, quizzes, and ATS resume analysis to offer a complete preparation kit',
+  keywords: [
+    'coding interviews',
+    'frontend practice',
+    'react quiz',
+    'live coding',
+    'developer tools',
+    'resume feedback',
+    'scan ats',
+    'DivBrainers',
+    'AI interview',
+    'mock interview',
+    'open-source',
+  ],
+  metadataBase: new URL('https://divbrainers.com'),
+  authors: [{ name: 'Stevan Dev', url: 'https://divbrainers.com' }],
+  creator: 'DivBrainers',
+  icons: {
+    icon: '/favicon-education.svg',
+  },
+  openGraph: {
+    title: 'DivBrainers — AI Tools for Developer Interviews',
+    description:
+      'Master coding interviews with quizzes, mock interviews, and AI-generated feedback on your resume — all in one place with DivBrainers.',
+    url: 'https://divbrainers.com',
+    siteName: 'DivBrainers',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'DivBrainers platform preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+    },
+  },
 };
 
 export default function RootLayout({
